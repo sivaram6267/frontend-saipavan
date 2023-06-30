c3a7543702d02fa5fcd775a6c816c84cb3acfdc4
sivaram6267 <90128892+sivaram6267@users.noreply.github.com>
2023-06-30 15:26:35 +0530

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -13,7 +13,7 @@ import axios from "axios";
 // const BASE_URL = "http://10.81.4.191:2030"; // sudheer pc
 //  const BASE_URL = "http://10.81.4.23:2030"; // server pc
 //const BASE_URL = "http://localhost:2032"
-const BASE_URL = "http://10.81.4.195:2020";
+const BASE_URL = "http://54.241.64.25:8080/LESM-Status-Monitor-0.0.1-SNAPSHOT";
 
 //sowmya pc
 // const BASE_URL = "http://10.81.4.197:2021"; // chamu pc
